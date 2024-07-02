87baada6a13bd97d3e19226d8d7f2ace76d3d292
rabiya.abubakar <rabiya.abubakar1student.moringaschool.com>
2024-07-02 17:10:29 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+// Define the saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+// Define the mondayWork function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;  // Ensure the period is included only here
+}
+
+// Define the wrapAdjective function 
+function wrapAdjective(highlight = '*') {
+    return function(adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`;
+    }
+}
+
+// Export the functions
+module.exports = { saturdayFun, mondayWork, wrapAdjective };
+
